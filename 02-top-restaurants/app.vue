6682eff028497b0a6f6fa1d4bfc1cb1802638061
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-08-18 14:37:12 -0700

    Added nav and front page

--- 02-top-restaurants/app.vue
+++ 02-top-restaurants/app.vue
@@ -1,4 +1,10 @@
 <template>
-  <div>
+  <Nav></Nav>
+
+  <div class="container text-center">
+    <h1 class="mt-5">Welcome to Restaurantly</h1>
+
+    <a href="/restaurants">Go to restaurants ></a>
   </div>
+
 </template>
